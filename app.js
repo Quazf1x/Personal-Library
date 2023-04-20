cf83857cb8efeb72f9cf326daf68148d983aeac5
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-04-20 19:15:03 +0600

    Ver 0.2.1

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
 const addBookBtn = document.querySelector('#add-book-button');
 const addBookWindow = document.querySelector('.add-book-popup');
 const overlay = document.querySelector('.overlay');
-const closeBookWindow = document.querySelector('#add-book-popup-close');
+const closeBookWindowBtn = document.querySelector('#add-book-popup-close');
+const addBookToLibraryBtn = document.querySelector('#popup-add-btn');
 
 let userLibrary = [];
 
-function Book(name,author,pagesCount,isRead){
-  this.name = name;
-  this.author = author;
-  this.pagesCount = pagesCount;
-  if(isRead) this.readStatus = true;
-  else this.readStatus = false;
+class Book{
+  constructor(
+    name = 'Unknown',
+    author = 'Unknown',
+    pagesCount = 0,
+    isRead = false
+    ) {
+    this.name = name;
+    this.author = author;
+    this.pagesCount = pagesCount;
+    this.readStatus = isRead;
+  }
 }
 
-function addBookToLibrary(){
+const addBookToLibrary = (book) => {
+  userLibrary.push(book);
 } 
 
-addBookBtn.addEventListener('click',()=>{
+const openBookWindow = () => {
   overlay.classList.add('popup-active');
   addBookWindow.classList.add('popup-active');
-})
+}
 
-closeBookWindow.addEventListener('click',()=>{
+const closeBookWindow = () => {
   overlay.classList.remove('popup-active');
   addBookWindow.classList.remove('popup-active');
+}
+
+const getBookFromInput = () =>{
+  let name = document.querySelector('#name-input').value;
+  let author = document.querySelector('#author-input').value;
+  let pagesCount = document.querySelector('#pages-input').value;
+  let status = document.querySelector('#read-status').checked;
+
+  return new Book(name, author, pagesCount, status);
+}
+
+addBookBtn.addEventListener('click',()=>{
+  openBookWindow();
+})
+
+closeBookWindowBtn.addEventListener('click',()=>{
+  closeBookWindow();
+})
+
+addBookToLibraryBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  const newBook = getBookFromInput();
+  addBookToLibrary(newBook);
+  console.log(userLibrary);
+  closeBookWindow();
 })
