84a8071bbe0ffd08b34133af1bafc3226f32676b
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-04-25 21:36:25 +0600

    Ver 4.5.1 FINAL(?) and functional

--- app.js
+++ app.js
@@ -4,40 +4,40 @@ const addBookWindow = document.querySelector('.add-book-popup');
 const overlay = document.querySelector('.overlay');
 const closeBookWindowBtn = document.querySelector('#add-book-popup-close');
 const addBookToLibraryBtn = document.querySelector('#popup-add-btn');
-const windowForm= document.querySelector('.add-book-popup');
+const windowForm = document.querySelector('.add-book-popup');
 
 let userLibrary = [];
 
 class Book { // The book constructor
   constructor(
-  name = 'Unknown',
-  author = 'Unknown',
-  pagesCount = 0,
-  isRead = false
+    name = 'Unknown',
+    author = 'Unknown',
+    pagesCount = 0,
+    isRead = false
   ) {
-  this.name = name;
-  this.author = author;
-  this.pagesCount = pagesCount;
-  this.readStatus = isRead;
+    this.name = name;
+    this.author = author;
+    this.pagesCount = pagesCount;
+    this.readStatus = isRead;
   }
 }
 
-const addBookToLibrary = (book) => {
+const addBookToLibrary = (book) => { //add book to the library object array
   userLibrary.push(book);
-} 
-
-const openBookWindow = () => { //opens the modal window
-  overlay.classList.add('popup-active');
-  addBookWindow.classList.add('popup-active');
-  windowForm.reset();  
 }
 
-const closeBookWindow = () => { //closes the modal window
+const openBookWindow = () => { // opens the modal window
+  overlay.classList.add('popup-active');
+  addBookWindow.classList.add('popup-active');
+  windowForm.reset();
+}
+
+const closeBookWindow = () => { // closes the modal window
   overlay.classList.remove('popup-active');
   addBookWindow.classList.remove('popup-active');
 }
 
-const getBookFromInput = () =>{ //collect values from user input
+const getBookFromInput = () => { // collect values from user input
   let name = document.querySelector('#name-input').value;
   let author = document.querySelector('#author-input').value;
   let pagesCount = document.querySelector('#pages-input').value;
@@ -49,20 +49,20 @@ const getBookFromInput = () =>{ //collect values from user input
 const submitBook = () => {  // function that fires after the submit button is pressed
   const newBook = getBookFromInput();
   addBookToLibrary(newBook);
-  createBookCard(newBook,(userLibrary.length - 1));
+  createBookCard(newBook, (userLibrary.length - 1));
   closeBookWindow();
 }
 
-const findBook = (name, array) => {
-  for(book of array){
-    if (book.name ===name)
-    return array.indexOf(book);
+const findBook = (name, array) => { //find the book by it's name
+  for (book of array) {
+    if (book.name === name)
+      return array.indexOf(book);
   }
 }
 
-const  deleteBook = (currentBook) => {
+const deleteBook = (currentBook) => { //deletes the found book from library array
   userLibrary.splice(currentBook, 1);
-} 
+}
 
 const createBookCard = (book) => {
 
@@ -81,24 +81,24 @@ const createBookCard = (book) => {
   const bookStatusWrapper = document.createElement('div');
   const bookStatus = document.createElement('button');
 
- //Adding classes
+  //Adding classes
 
   bookCard.classList.add('book-card');
   bookCardDeleteBtn.classList.add('delete-button');
-  bookCardDeleteBtn.setAttribute('src','img/close.svg');
+  bookCardDeleteBtn.setAttribute('src', 'img/close.svg');
 
   bookName.classList.add('book-property');
   bookAuthor.classList.add('book-property');
   bookPages.classList.add('book-property');
 
   bookStatus.classList.add('book-status');
+
   //Filling the elements
 
-  
   bookName.textContent = `Name: ${book.name}`;
-  
+
   bookAuthor.textContent = `Author: ${book.author}`;
-  
+
   bookPages.textContent = `Pages: ${book.pagesCount}`;
 
 
@@ -122,30 +122,30 @@ const createBookCard = (book) => {
     bookStatusWrapper);
   bookGrid.appendChild(bookCard);
 
-  bookCardDeleteBtn.addEventListener('click', (e) => {
+  bookCardDeleteBtn.addEventListener('click', (e) => { //removes the book from DOM and Library
     deleteBook(findBook(book.name, userLibrary));
     e.target.parentNode.remove();
-  }); 
+  });
 
-  bookStatus.addEventListener('click', (e)=>{
+  bookStatus.addEventListener('click', (e) => { // toggle the read-status for the selected book
     book.readStatus = !book.readStatus;
 
     const currentBook = e.target.parentNode.parentNode;
     bookStatus.classList.toggle('status-read');
     currentBook.classList.toggle('book-read');
-    if(currentBook.classList.contains('book-read')) 
+    if (currentBook.classList.contains('book-read'))
       bookStatus.innerHTML = 'Read';
-      
+
     else
       bookStatus.innerHTML = 'Not Read';
   });
-} 
+}
 
-addBookBtn.addEventListener('click',()=>{
+addBookBtn.addEventListener('click', () => {
   openBookWindow();
 })
 
-closeBookWindowBtn.addEventListener('click',()=>{
+closeBookWindowBtn.addEventListener('click', () => {
   closeBookWindow();
 })
 
